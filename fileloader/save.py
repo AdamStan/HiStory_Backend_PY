ce7fd2d839ac8f1613930c457bd129ea946b1c07
AdamStan <adam15stan@gmail.com>
2021-07-04 20:06:29 +0200

    Code refactor for SaveQuestionsFromFile

--- fileloader/save.py
+++ fileloader/save.py
@@ -9,25 +9,32 @@ class SaveQuestionsFromFile:
 
     def save_questions(self):
         for question in self.questions:
-
-            type_from_db = AnswerType.objects.filter(type = question.correct_answer.type.type)
-            if len(type_from_db) > 0:
-                question.correct_answer.type = type_from_db[0]
-            else:
-                question.correct_answer.type.save()
-
-            category_from_db = Category.objects.filter(category = question.correct_answer.category.category)
-            if len(category_from_db) > 0:
-                question.correct_answer.category = category_from_db[0]
-            else:
-                question.correct_answer.category.save()
-            
-            answer_from_db = Answer.objects.filter(answer = question.correct_answer.answer, 
-                                                   type = question.correct_answer.type, 
-                                                   category = question.correct_answer.category)
-            if len(answer_from_db) > 0:
-                question.correct_answer = answer_from_db[0]
-            else:
-                question.correct_answer.save()
+            question.correct_answer.type = self.save_type_without_repetition(question.correct_answer.type)
+            question.correct_answer.category = self.save_category_without_repetition(question.correct_answer.category)
+            question.correct_answer = self.save_answer_without_repetition(question.correct_answer)
 
             question.save()
+
+    def save_type_without_repetition(self, answer_type):
+        type_from_db = AnswerType.objects.filter(type = answer_type.type)
+        if len(type_from_db) > 0:
+            return type_from_db[0]
+        answer_type.save()
+        return answer_type
+
+    def save_category_without_repetition(self, category):
+        category_from_db = Category.objects.filter(category = category.category)
+        if len(category_from_db) > 0:
+            return category_from_db[0]
+        category.save()
+        return category
+
+    def save_answer_without_repetition(self, answer):
+        answer_from_db = Answer.objects.filter(answer = answer.answer, 
+                                               type = answer.type, 
+                                               category = answer.category)
+        if len(answer_from_db) > 0:
+            return answer_from_db[0]
+        answer.save()
+        return answer
+    
