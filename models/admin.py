f99d146b30eeb74f955e986420e03b3ade182c7b
AdamStan <adam15stan@gmail.com>
2021-09-05 17:12:45 +0200

    adding questions from files now works from admin sites

--- models/admin.py
+++ models/admin.py
@@ -1,3 +1,4 @@
+from models.views import upload_file
 from models.models import Answer, AnswerType, Category, Question
 from django.contrib import admin
 
@@ -9,14 +10,32 @@ class QuestionAdmin(admin.ModelAdmin):
 class QuestionAdminLoadingFromFile(QuestionAdmin):
     add_form_template = "upload_admin.html"
 
+    def _changeform_view(self, request, object_id, form_url, extra_context):
+        if object_id:
+            print("Question update!")
+        else:
+            print("FILE Upload")
+            if upload_file(request):
+                obj_with_meta = QuestionsFromFile()
+                obj_with_meta.name_of_file = str(request.FILES['file'])
+                return self.response_add(request, obj_with_meta)
+        return super(QuestionAdminLoadingFromFile, self)._changeform_view(
+            request, object_id, form_url, extra_context)
+
 
 class QuestionsFromFile(Question):
     class Meta:
         proxy = True
 
+    name_of_file = "DON't click!"
+
+    def __str__(self):
+        return self.name_of_file
+
 
 class AnswerAdmin(admin.ModelAdmin):
     list_display = ['answer', 'category_name']
+
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
